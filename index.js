75643ef62c8a03c17e6684a17f90e89c7fd2bf59
Dendy <dendy@mmi-pt.com>
2024-03-18 15:38:16 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const readExcelFile = require("./libs/excel-reader");
 const testLinkRedirect = require("./libs/link-checker");
 const { writeCsvFile } = require("./libs/excel-writer");
+require("dotenv").config();
 
-const SOURCE_URL = "Target/As-is URL";
-const DESTINATION_URL = "Destination/To-be URL (Expected)";
+const COL_SOURCE_URL = process.env.COL_SOURCE_URL || "Target/As-is URL";
+const COL_DESTINATION_URL =
+  process.env.COL_DESTINATION_URL || "Destination/To-be URL (Expected)";
+
+console.log(process.env.SOURCE_FILE);
 
 async function start() {
-  const data = await readExcelFile("./santen-rewrite-test.xlsx");
+  const data = await readExcelFile(process.env.SOURCE_FILE);
 
   let resultData = [];
 
   for (var i = 0; i < data.length; i++) {
     const row = data[i];
-    const source = row[SOURCE_URL];
-    const destination = row[DESTINATION_URL];
+    const source = row[COL_SOURCE_URL];
+    const destination = row[COL_DESTINATION_URL];
 
     if (source && destination) {
       const result = await testLinkRedirect(source, destination);
@@ -24,9 +28,7 @@ async function start() {
     }
   }
 
-  //   console.log(resultData);
-
-  await writeCsvFile(resultData, "./santen-rewrite-result.csv");
+  await writeCsvFile(resultData, "./result.csv");
 }
 
 start();
